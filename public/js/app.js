684abb4bf554603c38a8d626a94eefc6ff3b41ab
urizennnnnn <igamerryt@gmail.com>
2023-10-05 22:26:47 +0100

    added sockets emitter on both ends

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,55 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+    // Check for browser support
+    const SpeechRecog = window.SpeechRecognition || window.webkitSpeechRecognition;
+    if (!SpeechRecog) {
+        console.error("SpeechRecognition API is not supported in this browser.");
+        // Handle unsupported browser here
+        return;
+    }
 
-const SpeechRecog = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecog();
+    recognition.lang = 'en-US';
+    recognition.interimResults = false;
 
-const recognition = new SpeechRecog();
-recognition.lang = 'en-US';
-recognition.interimResults = false
-const mic = document.querySelector('.mic');
-const socket = io();
+    const mic = document.querySelector('.mic');
 
+    // Add Socket.io configuration if you're using it
+    const socket = io();
 
-mic.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log('Clicked')
-    recognition.start();
+    function apiVoiceResponse(text) {
+        const synth = window.speechSynthesis;
+        const utterance = new SpeechSynthesisUtterance();
+        utterance.text = text;
+        utterance.lang = 'en-US'; // Set the desired language
+        synth.speak(utterance);
+    }
+
+    mic.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log('Clicked');
+        recognition.start();
+    });
+
+    recognition.addEventListener('result', (e) => {
+        const last = e.results.length - 1;
+        const text = e.results[last][0].transcript.toString()
+        console.log('I said: ' + text);
+
+        // Emit the recognized text to the server if using Socket.io
+        socket.emit('chat message', text);
+    });
+
+    recognition.addEventListener('end', () => {
+        console.log('Speech recognition ended');
+    });
+
+    // Add error handling for speech recognition
+    recognition.addEventListener('error', (event) => {
+        console.error('Speech recognition error:', event.error);
+    });
+
+    // Handle the response from the server using Socket.io
+    socket.on('bot reply', function (replyText) {
+        apiVoiceResponse(replyText);
+    });
 });
-
-recognition.addEventListener('result', (e) => {
-    const last = e.results.length - 1;
-    const text = e.results[last][0].transcript;
-    console.log('I said: ' + text);
-
-    socket.emit('chat message', text);
-
-});
-
-recognition.addEventListener('speechstart', () => {
-    console.log('Speech recognition started');
-});
-
-recognition.addEventListener('speechend', () => {
-    console.log('Speech recognition ended');
-});
-
--- server.js
+++ server.js
@@ -1,11 +1,61 @@
 const express = require("express");
 const app = express();
 const morgan = require('morgan');
-const path = require('path'); 
+const path = require('path');
+const { SessionsClient } = require('@google-cloud/dialogflow'); 
+const uuid = require('uuid');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.use(morgan('dev'));
+const projectId = process.env.PROJECT_ID;
+const credentials = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+const sessionClient = new SessionsClient({
+    projectId: projectId,
+    credentials: credentials
+});
+
+async function detectIntent(projectId, text) {
+    const sessionId = uuid.v4();
+
+    const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: text,
+                languageCode: 'en-US',
+            },
+        },
+    };
+
+    try {
+        const responses = await sessionClient.detectIntent(request);
+        const result = responses[0].queryResult;
+        return result.fulfillmentText;
+    } catch (error) {
+        console.error('Error communicating with Dialogflow:', error.message);
+        return 'Sorry, there was an error.';
+    }
+}
+io.on('connection', function (socket) {
+    socket.on('chat message', async (text) => {
+        try {
+            console.log('text')
+            const aiText = await detectIntent(projectId, text);
+            socket.emit('bot reply', aiText); 
+        } catch (error) {
+            console.log(error);
+            socket.emit('bot reply', "I couldn't find a reply Victor sama.");
+        }
+    });
+});
 
 const PORT = process.env.PORT || 4000;
+
+// Serve static files
 app.use(express.static(path.join(__dirname, 'public'), {
     setHeaders: (res, path) => {
         if (path.endsWith('.css')) {
@@ -17,12 +67,17 @@ app.use(express.static(path.join(__dirname, 'public'), {
     }
 }));
 
-app.use(express.static(path.join(__dirname, 'views'))); 
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html')); 
+app.use(express.static(path.join(__dirname, 'views')));
+app.get('/test', (req, res) => {
+    console.log(process.env.PROJECT_ID);
+    res.status(200).json('check');
 });
 
-app.listen(PORT, () => {
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Call the detectIntent function when the server starts
+http.listen(PORT, () => {
     console.log(`Server listening on localhost:${PORT}`);
 });
